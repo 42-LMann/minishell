44cc7946206ee857bcc3ad9aa4aeaa4829a74c8d
Lachlan Mann <lmann@F3C022.42adel.org.au>
2022-08-19 15:02:51 +0930

    change to input process

--- includes/defines.h
+++ includes/defines.h
@@ -6,7 +6,7 @@
 /*   By: lmann <lmann@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/20 16:17:19 by lmann             #+#    #+#             */
-/*   Updated: 2022/06/20 16:30:23 by lmann            ###   ########.fr       */
+/*   Updated: 2022/08/19 14:49:47 by lmann            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,8 @@
 
 #include "minishell.h"
 
+
+# define PROMPT "Minishell $"
 # define STDIN 0
 # define STDOUT 1
 # define STDERR 2
--- srcs/main.c
+++ srcs/main.c
@@ -6,7 +6,7 @@
 /*   By: lmann <lmann@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/20 14:46:30 by lmann             #+#    #+#             */
-/*   Updated: 2022/08/01 16:54:09 by lmann            ###   ########.fr       */
+/*   Updated: 2022/08/19 14:56:41 by lmann            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,11 +23,11 @@ void	print_text_prompt(void)
 	write(1, RESET, 7);
 }
 
-t_err	process_input(char *line, t_curr_input *input, t_envi *info)
+t_err	process_input(char *line, t_curr_input *g_cmdline, t_envi *info)
 {
 	int						i;
 	t_err					err;
-	static t_proc_input_ptr	proc_funptr[3] = {
+	static t_proc_cmd_line	proc_funptr[3] = {
 	[0] = lexer,
 	[1] = expander,
 	[2] = parser
@@ -36,7 +36,7 @@ t_err	process_input(char *line, t_curr_input *input, t_envi *info)
 	i = 0;
 	while (i < 3)
 	{
-		err = proc_funptr[i](line, input, info);
+		err = proc_funptr[i](line, g_cmdline, info);
 		if (err != NO_ERROR)
 		{
 			if (err == SYNTAX_ERR)
@@ -45,48 +45,31 @@ t_err	process_input(char *line, t_curr_input *input, t_envi *info)
 		}
 		i++;
 	}
-	clean_lexer(&input->lexer);
+	clean_lexer(&g_cmdline->lexer);
 	return (NO_ERROR);
 }
 
-t_err	get_input(t_envi *envi, char **input_ptr)
+void	default_term(t_envi *envi)
 {
-	char	*temp;
-
 	set_term_settings();
 	rl_replace_line("", 0);
-	*input_ptr = readline(YEL PROMPT RESET);
+	g_cmdline = readline(GREEN PROMPT RESET);
 	restore_term_settings(&envi->termios_p);
-	if (*input_ptr && (*input_ptr)[0])
-	{
-		temp = *input_ptr;
-		*input_ptr = ft_strtrim_white(*input_ptr);
-		free(temp);
-		if (!*input_ptr)
-			return (MALLOC_FAIL);
-		add_history(*input_ptr);
-	}
-	//else if (!*input_ptr)
-		//ft_exit_sig(envi);
-	return (NO_ERROR);
+	add_history(g_cmdline);
 }
 
 int	loop_mini(t_envi *envi, t_token *token_list)
 {
-	char	*input;
-	int		status = 0;
 	t_err	rv;
 
-	g_cmdline = readline(">>> ");
-	while (status == 0 || g_cmdline != NULL)
+	while (1)
 	{
-		init_sigs();
-		if (get_input(envi, g_cmdline) == MALLOC_FAIL)
-			return (-1);
-		if (!input)
+		//init_sigs();
+		default_term(envi);
+		if (!g_cmdline)
 			continue;
-		rv = process_input(input, token_list, envi);
-		free (input)
+		rv = process_input(g_cmdline, token_list, envi);
+		free (g_cmdline)
 		if (rv != NO_ERROR)
 		{
 			clean_tree(&token_list->tree)
@@ -95,7 +78,7 @@ int	loop_mini(t_envi *envi, t_token *token_list)
 				exit (-1);
 			continue;
 		}
-		ignore_sigs();
+		//ignore_sigs();
 		ft_start_tree(envi, &token_list->tree);
 	}
 }
